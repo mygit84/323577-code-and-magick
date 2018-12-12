3ae2c93591d42eb124b72ac32bef68802346df2e
Наталья Осокина <amnevsepo84@mail.ru>
2018-12-12 15:02:04 +0300

    Добавила работу с сервером

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@
   };
   var userDialog = document.querySelector('.setup');
   var userName = userDialog.querySelector('.setup-user-name');
+  var form = userDialog.querySelector('.setup-wizard-form');
 
   var openPopup = function () {
     userDialog.classList.remove('hidden');
@@ -22,26 +23,32 @@
     document.removeEventListener('keydown', onPopupEscPress);
   };
 
-  var onPopupEscPress = function (evt) {
-    if (window.setup.isEscEvent(evt)) {
-      if (userName === document.activeElement) {
-        return evt;
-      } else {
-        closePopup();
-      }
+  var getActiveElement = function (evt) {
+    if (userName === document.activeElement) {
+      return evt;
+    } else {
+      closePopup();
     }
     return evt;
+  };
+
+  var onPopupEscPress = function (evt) {
+    if (window.setup.isEscEvent(evt)) {
+      getActiveElement();
+    }
   };
 
   window.setup.setOpen(openPopup);
   window.setup.setClose(closePopup);
 
-  var initWizards = window.wizards.getWizards(window.randomParam.getEyesColor(),
-      window.randomParam.getCoatColor(),
-      window.randomParam.fullName());
-  window.wizards.drawWizards(initWizards);
-
   window.wizardSetting.onClickCoat(window.randomParam.coatColor);
   window.wizardSetting.onClickEyes(window.randomParam.eyesColor);
   window.wizardSetting.onClickFireball(window.randomParam.fireballColor);
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), closePopup, window.error.onError);
+    evt.preventDefault();
+  });
+
+  window.backend.load(window.wizards.onLoad, window.error.onError);
 })();
